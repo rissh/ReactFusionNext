171bcd615746b3d7a42dcf9c3cc30675e64e0e82
Rishikesh <rishikeshjagadale19@gmail.com>
2024-08-24 20:09:04 +0530

    Added Header Component

--- trackmate/src/components/Header.js
+++ trackmate/src/components/Header.js
@@ -1,7 +1,33 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="title">
+        <h1>Active Sprint</h1>
+      </div>
+      <div className="actions">
+        <button className="complete_sprint_button">Complete Sprint</button>
+        <button className="more_options">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+            />
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
